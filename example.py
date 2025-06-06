678734264bf9be3ebaa0cd173bf6a94b2d0f2510
Gudipati Umesh <150164672+umesh530731@users.noreply.github.com>
2025-06-06 08:52:32 +0530

    example.py

--- example.py
+++ example.py
@@ -29,13 +29,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model
-
-df=pd.read_excel(r"C:\web dev\Intern-DSR\rangevsbatterycap.xlsx")
+import os
+base_dir = os.path.dirname(__file__)
+file_path = os.path.join(base_dir, 'rangevsbatterycap.xlsx')
+df=pd.read_excel(file_path)
 print(df)
 
 #%matplotlib inline
 plt.scatter(df.Range,df.BatteryCapacity,color='red',marker='.')
 plt.xlabel('Range')
 plt.ylabel('BatteryCapacity')
-
+plt.title('Range vs Battery Capacity')
+plt.grid(True)
 plt.show()
